9a6e6eef3477125275eebc62d926660ad72bab75
WooLyung <vlfkdydtk@naver.com>
2023-05-06 22:01:13 +0900

    TCP 서버 기본

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 1025
+
+s.bind((host, port))
+s.listen(5)
+
+print("Server started!")
+
+while True:
+    c, addr = s.accept()
+    print('joined', addr)
+    c.close()
