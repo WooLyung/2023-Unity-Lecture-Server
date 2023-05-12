c3989c8985fc2f2d82fd2c78846beddd0d5ba8d5
WooLyung <vlfkdydtk@naver.com>
2023-05-12 19:58:03 +0000

    1일차 서버 끝.. 아마도?

--- main.py
+++ main.py
@@ -4,56 +4,79 @@ import time
 from _thread import *
 
 joins = dict()
-event_queue = list()
+evt_queue = list()
 
 def make_data(**datas):
     return datas
 
-def event_thread():
+def evt_thread():
     while True:
-        while len(event_queue) > 0:
-            evt = event_queue.pop(0)
-            if evt['event'] == 'join':
-                print('join event')
-            elif evt['event'] == 'quit':
-                del joins[evt['id']]
+        while len(evt_queue) > 0:
+            evt = evt_queue.pop(0)
+            print(evt)
+            if evt['evt'] == 'join':
+                ''
+            elif evt['evt'] == 'quit':
+                if evt['id'] in joins.keys():
+                    try:
+                        joins[evt['id']].close()
+                    except Exception:
+                        ''
+                    del joins[evt['id']]
+            elif evt['evt'] == 'update':
+                if evt['id'] in joins.keys():
+                    joins[evt['id']]['x'] = evt['x']
+                    joins[evt['id']]['y'] = evt['y']
+                    joins[evt['id']]['angle'] = evt['angle']
         for id, player in joins.items():
-            print(id, player)
+            players = []
+            for id2, player2 in joins.items():
+                if id != id2:
+                    players.append(make_data(id=id2, hp=player2['hp'], x=player2['x'], y=player2['y'], angle=player2['angle']))
+            player['sock'].send(str(json.dumps(make_data(evt='update', hp=player['hp'], others=players)) + "#").encode())
         time.sleep(0.05)
 
 def threaded(client_socket, addr, id):
     print('client connected: ', addr[0], ':', addr[1], sep='')
-    client_socket.settimeout(1)
     while True:
         try:
             data = client_socket.recv(1024)
             if not data:
-                # print('client disconnected: ', addr[0], ':', addr[1], sep='')
-                event_queue.append(make_data(event='quit', id=id))
+                evt_queue.append(make_data(evt='quit', id=id))
                 break
-            # print('Received from ' + addr[0], ':', addr[1], data.decode())
-            jsonData = json.loads(data.decode())
-            if jsonData['event'] == 'join':
-                if 'nickname' in jsonData.keys() and 'color' in jsonData.keys():
-                    client_socket.send(json.dumps(make_data(result='success')).encode())
-                    event_queue.append(make_data(event='join', id=id))
-                    nickname = jsonData['nickname']
-                    color = jsonData['color']
-                    joins[id] = make_data(nickname=nickname, color=color, hp=100, sock=client_socket)
-                else:
-                    client_socket.send(json.dumps(make_data(result='fail')).encode())
+            for data in data.decode()[:-1].split('#'):
+                jsonData = json.loads(data)
+                keys = jsonData.keys()
+                if jsonData['evt'] == 'join':
+                    if 'nickname' in keys and 'color' in keys:
+                        client_socket.send(json.dumps(make_data(result='success')).encode())
+                        evt_queue.append(make_data(evt='join', id=id))
+                        nickname = jsonData['nickname']
+                        color = jsonData['color']
+                        x = 0.0
+                        y = 0.0
+                        angle = 0.0
+                        joins[id] = make_data(nickname=nickname, color=color, hp=100, x=x, y=y, angle=angle, sock=client_socket)
+                    else:
+                        client_socket.send(json.dumps(make_data(result='fail')).encode())
+                if jsonData['evt'] == 'update':
+                    if 'x' in keys and 'y' in keys and 'angle' in keys:
+                        x = jsonData['x']
+                        y = jsonData['y']
+                        angle = jsonData['angle']
+                        evt_queue.append(make_data(evt='update', id=id, x=x, y=y, angle=angle))
 
-        except ConnectionResetError as e:
-            # print('client disconnected: ', addr[0], ':', addr[1], sep='')
-            event_queue.append(make_data(event='quit', id=id))
+        except ConnectionResetError:
+            evt_queue.append(make_data(evt='quit', id=id))
             break
 
         except socket.timeout:
-            event_queue.append(make_data(event='quit', id=id))
+            evt_queue.append(make_data(evt='quit', id=id))
             break
 
         except Exception as e:
             print('client error: ', addr[0], ':', addr[1], ' : ', e, sep='')
+
     client_socket.close()
 
 HOST = ''
@@ -66,7 +89,8 @@ server_socket.listen()
 
 print('server start')
 
-start_new_thread(event_thread, ())
+socket.setdefaulttimeout(1)
+start_new_thread(evt_thread, ())
 
 id = 0
 while True:
