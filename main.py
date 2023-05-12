34c85ffa9aefbe68942ae5e4816547a3fed3862b
WooLyung <vlfkdydtk@naver.com>
2023-05-12 22:34:29 +0900

    멀티스레드 에코 서버

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import socket
+from _thread import *
 
-s = socket.socket()
-host = socket.gethostname()
-port = 1025
+def threaded(client_socket, addr):
+    print('client connected: ', addr[0], ':', addr[1], sep='')
+    while True:
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                print('client disconnected: ', addr[0], ':', addr[1], sep='')
+                break
+            print('Received from ' + addr[0], ':', addr[1], data.decode())
+            # client_socket.send(data)
+        except ConnectionResetError as e:
+            print('client disconnected: ', addr[0], ':', addr[1], sep='')
+            break
+    client_socket.close()
 
-s.bind((host, port))
-s.listen(5)
+HOST = '127.0.0.1'
+PORT = 9172
 
-print("Server started!")
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT))
+server_socket.listen()
 
+print('server start')
 while True:
-    c, addr = s.accept()
-    print('joined', addr)
-    c.close()
+    client_socket, addr = server_socket.accept()
+    start_new_thread(threaded(client_socket, addr))
+
+server_socket.close()
